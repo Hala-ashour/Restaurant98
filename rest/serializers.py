5e4cd5d0d81d94514824dba248bcf4a3839d4694
Ahmad-bulbul <ahmadbulbul06@gmail.com>
2025-07-28 16:49:45 +0300

    new

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from rest_framework import viewsets
-from .models import Category
+from .models import Category ,Order
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields=['id','name','description','is_active']
+
+
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['customer','order_date','total_amount','status','notes']
+
+
--- rest/views.py
+++ rest/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer,OrderSerializer
+from .models import Category,Order
 from rest_framework import viewsets,status
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
 
 
 class IsAdminOrManager(BasePermission):
@@ -24,3 +26,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return [IsAdminOrManager()]
         return [IsAuthenticated()]  
+
+
+
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+
+class orderviewset(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'customer']
